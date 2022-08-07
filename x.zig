bbefc4498fddc0eb90a7038bba698b82a6781e59
Jonathan Marler <johnnymarler@gmail.com>
2022-08-07 10:46:56 -0600

    move Slice into its own module

--- x.zig
+++ x.zig
@@ -35,6 +35,7 @@ pub const inputext = @import("xinputext.zig");
 // Expose some helpful stuff
 pub const Memfd = @import("Memfd.zig");
 pub const ContiguousReadBuffer = @import("ContiguousReadBuffer.zig");
+pub const Slice = @import("x/slice.zig").Slice;
 
 pub const TcpBasePort = 6000;
 
@@ -354,53 +355,6 @@ pub fn connectUnix(display_host: ?[]const u8, display_num: u32) !os.socket_t {
 //    proto_major_version : u16,
 //    proto_minor_version : u16,
 //};
-
-pub fn Slice(comptime LenType: type, comptime Ptr: type) type { return struct {
-    const Self = @This();
-    const ptr_info = @typeInfo(Ptr).Pointer;
-    pub const NativeSlice = @Type(std.builtin.TypeInfo {
-        .Pointer = .{
-            .size = .Slice,
-            .is_const = ptr_info.is_const,
-            .is_volatile = ptr_info.is_volatile,
-            .alignment = ptr_info.alignment,
-            .address_space = ptr_info.address_space,
-            .child = ptr_info.child,
-            .is_allowzero = ptr_info.is_allowzero,
-            .sentinel = ptr_info.sentinel,
-        },
-    });
-
-    ptr: Ptr,
-    len: LenType,
-
-    pub fn nativeSlice(self: @This()) NativeSlice {
-        return self.ptr[0 .. self.len];
-    }
-
-    pub fn initComptime(comptime ct_slice: NativeSlice) @This() {
-        return .{ .ptr = ct_slice.ptr, .len = @intCast(LenType, ct_slice.len) };
-    }
-
-    pub fn lenCast(self: @This(), comptime NewLenType: type) Slice(NewLenType, Ptr) {
-        return .{ .ptr = self.ptr, .len = @intCast(NewLenType, self.len) };
-    }
-
-    pub usingnamespace switch (@typeInfo(Ptr).Pointer.child) {
-        u8 => struct {
-            pub fn format(
-                self: Self,
-                comptime fmt: []const u8,
-                options: std.fmt.FormatOptions,
-                writer: anytype,
-            ) !void {
-                _ = fmt; _ = options;
-                try writer.writeAll(self.ptr[0 .. self.len]);
-            }
-        },
-        else => struct {},
-    };
-};}
 
 pub fn ArrayPointer(comptime T: type) type {
     const err = "ArrayPointer not implemented for " ++ @typeName(T);
---
+++ x/slice.zig
@@ -0,0 +1,48 @@
+const std = @import("std");
+
+pub fn Slice(comptime LenType: type, comptime Ptr: type) type { return struct {
+    const Self = @This();
+    const ptr_info = @typeInfo(Ptr).Pointer;
+    pub const NativeSlice = @Type(std.builtin.TypeInfo {
+        .Pointer = .{
+            .size = .Slice,
+            .is_const = ptr_info.is_const,
+            .is_volatile = ptr_info.is_volatile,
+            .alignment = ptr_info.alignment,
+            .address_space = ptr_info.address_space,
+            .child = ptr_info.child,
+            .is_allowzero = ptr_info.is_allowzero,
+            .sentinel = ptr_info.sentinel,
+        },
+    });
+
+    ptr: Ptr,
+    len: LenType,
+
+    pub fn nativeSlice(self: @This()) NativeSlice {
+        return self.ptr[0 .. self.len];
+    }
+
+    pub fn initComptime(comptime ct_slice: NativeSlice) @This() {
+        return .{ .ptr = ct_slice.ptr, .len = @intCast(LenType, ct_slice.len) };
+    }
+
+    pub fn lenCast(self: @This(), comptime NewLenType: type) Slice(NewLenType, Ptr) {
+        return .{ .ptr = self.ptr, .len = @intCast(NewLenType, self.len) };
+    }
+
+    pub usingnamespace switch (@typeInfo(Ptr).Pointer.child) {
+        u8 => struct {
+            pub fn format(
+                self: Self,
+                comptime fmt: []const u8,
+                options: std.fmt.FormatOptions,
+                writer: anytype,
+            ) !void {
+                _ = fmt; _ = options;
+                try writer.writeAll(self.ptr[0 .. self.len]);
+            }
+        },
+        else => struct {},
+    };
+};}
