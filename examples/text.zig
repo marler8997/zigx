81b47c5e6f6a47cb904802121db6bc45f1f2dd9f
Jonathan Marler <johnnymarler@gmail.com>
2026-01-20 18:15:54 -0700

    add cmdline options to text example and size slider

--- examples/text.zig
+++ examples/text.zig
@@ -28,6 +28,32 @@ const Ids = struct {
 };
 
 pub fn main() !void {
+    var arena_instance: std.heap.ArenaAllocator = .init(std.heap.page_allocator);
+    // no need to deinit
+    const cmdline = try Cmdline.alloc(arena_instance.allocator());
+
+    var opt: struct {
+        font_file: ?[]const u8 = null,
+        size: f32 = 24.0,
+    } = .{};
+
+    {
+        var i: usize = 1;
+        while (i < cmdline.len()) : (i += 1) {
+            const arg = cmdline.arg(i);
+            if (std.mem.eql(u8, arg, "--font")) {
+                i += 1;
+                if (i == cmdline.len()) errExit("--font missing arg", .{});
+                opt.font_file = cmdline.arg(i);
+            } else if (std.mem.eql(u8, arg, "--size")) {
+                i += 1;
+                if (i == cmdline.len()) errExit("--size missing arg", .{});
+                const size_str = cmdline.arg(i);
+                opt.size = std.fmt.parseFloat(f32, size_str) catch errExit("invalid --size '{s}'", .{size_str});
+            } else errExit("unknown cmdline option '{s}'", .{arg});
+        }
+    }
+
     try x11.wsaStartup();
 
     const Screen = struct {
@@ -67,7 +93,7 @@ pub fn main() !void {
     var sink: x11.RequestSink = .{ .writer = &socket_writer.interface };
     var source: x11.Source = .initAfterSetup(socket_reader.interface());
 
-    var window_size: XY(u16) = .{ .x = 600, .y = 300 };
+    var window_size: XY(u16) = .{ .x = 600, .y = 700 };
 
     try sink.CreateWindow(
         .{
@@ -114,13 +140,33 @@ pub fn main() !void {
 
     try sink.MapWindow(ids.window());
     const font_ids = ids.font();
-    const ttf: TrueType = try .load(@embedFile("InterVariable.ttf"));
+
+    const ttf_content = blk: {
+        if (opt.font_file) |font_file| {
+            break :blk std.fs.cwd().readFileAlloc(
+                arena_instance.allocator(),
+                font_file,
+                std.math.maxInt(usize),
+            ) catch |e| errExit(
+                "read '{s}' failed with {s}",
+                .{ font_file, @errorName(e) },
+            );
+        }
+        break :blk @embedFile("InterVariable.ttf");
+    };
+    const ttf: TrueType = try .load(ttf_content);
+    var font_size: f32 = opt.size;
     var font: Font = try .init(std.heap.page_allocator, &ttf, font_ids, .{
-        .size = 24,
+        .size = font_size,
         .color = 0xffffff,
     });
     defer font.deinit(std.heap.page_allocator, &sink) catch |err| @panic(@errorName(err));
     var glyph_arena: ArenaAllocator = .init(std.heap.page_allocator);
+
+    var sliding: bool = false;
+    var layout: Layout = .{
+        .slider = .{ .x = 0, .y = 0, .width = 0, .height = 0 },
+    };
 
     while (true) {
         try sink.writer.flush();
@@ -140,6 +186,29 @@ pub fn main() !void {
 
         var do_render = false;
         switch (msg_kind) {
+            .ButtonPress => {
+                const msg = try source.read2(.ButtonPress);
+                if (msg.button == 1) {
+                    const pt: XY(i16) = .{ .x = msg.event_x, .y = msg.event_y };
+                    if (rectContains(layout.slider, pt)) {
+                        sliding = true;
+                        do_render = try updateFontSize(&sink, &ttf, font_ids, &font_size, &font, layout.slider, pt.x);
+                    }
+                }
+            },
+            .ButtonRelease => {
+                const msg = try source.read2(.ButtonRelease);
+                if (msg.button == 1) {
+                    sliding = false;
+                }
+            },
+            .MotionNotify => {
+                const msg = try source.read2(.MotionNotify);
+                if (sliding) {
+                    const pt: XY(i16) = .{ .x = msg.event_x, .y = msg.event_y };
+                    do_render = try updateFontSize(&sink, &ttf, font_ids, &font_size, &font, layout.slider, pt.x);
+                }
+            },
             .Expose => {
                 const expose = try source.read2(.Expose);
                 std.log.info("X11 {}", .{expose});
@@ -155,9 +224,6 @@ pub fn main() !void {
                     do_render = true;
                 }
             },
-            .MotionNotify,
-            .ButtonPress,
-            .ButtonRelease,
             .MapNotify,
             .MappingNotify,
             .ReparentNotify,
@@ -165,17 +231,58 @@ pub fn main() !void {
             else => std.debug.panic("unexpected X11 {f}", .{source.readFmt()}),
         }
         if (do_render) {
-            try render(
+            layout = try render(
                 &glyph_arena,
                 &sink,
                 ids,
                 dbe,
                 &font,
                 window_size,
+                font_size,
+                opt.font_file,
             );
         }
     }
 }
+
+fn updateFontSize(
+    sink: *x11.RequestSink,
+    ttf: *const TrueType,
+    font_ids: Font.Ids,
+    font_size: *f32,
+    font: *Font,
+    slider_rect: x11.Rectangle,
+    x: i16,
+) !bool {
+    const new_font_size: f32 = blk: {
+        const offset: i32 = if (x < slider_rect.x) 0 else @intCast(x - slider_rect.x);
+        const slot = if (offset >= slider_rect.width) slider_rect.width else offset;
+        const ratio = @as(f32, @floatFromInt(slot)) / @as(f32, @floatFromInt(slider_rect.width));
+        break :blk font_min + (ratio * (font_max - font_min));
+    };
+    if (new_font_size == font_size.*) return false;
+    try font.deinit(std.heap.page_allocator, sink);
+    font.* = try .init(std.heap.page_allocator, ttf, font_ids, .{
+        .size = new_font_size,
+        .color = 0xffffff,
+    });
+    font_size.* = new_font_size;
+    return true;
+}
+
+const font_min: f32 = 1.0;
+const font_max: f32 = 100.0;
+
+fn rectContains(rect: x11.Rectangle, pt: XY(i16)) bool {
+    return pt.x >= rect.x and
+        pt.x < (rect.x + @as(i16, @intCast(rect.width))) and
+        pt.y >= rect.y and
+        pt.y < (rect.y + @as(i16, @intCast(rect.height)));
+}
+
+const Layout = struct {
+    slider: x11.Rectangle,
+};
 
 fn render(
     glyph_arena: *ArenaAllocator,
@@ -184,7 +291,9 @@ fn render(
     dbe: Dbe,
     font: *Font,
     window_size: XY(u16),
-) !void {
+    font_size: f32,
+    font_file: ?[]const u8,
+) !Layout {
     const window = ids.window();
     const gc = ids.gc();
 
@@ -200,9 +309,30 @@ fn render(
             .{ .exposures = false },
         );
     }
-    const drawable = if (dbe.backBuffer()) |back_buffer| back_buffer else window.drawable();
-    const margin = 50;
+    const drawable: x11.Drawable = if (dbe.backBuffer()) |back_buffer| back_buffer else window.drawable();
 
+    const slider_margin_left = 10;
+    const slider_rail_half_height = 2;
+    const slider_half_height = 9;
+    const slider_margin_top = 10;
+    const slider_width = 300;
+    const layout: Layout = .{
+        .slider = .{
+            .x = slider_margin_left,
+            .y = slider_margin_top,
+            .width = slider_width,
+            .height = slider_half_height * 2,
+        },
+    };
+    const slider_ratio: f32 = (font_size - font_min) / font_max;
+    const slider_pos: i32 = @intFromFloat(@round(@as(f32, @floatFromInt(layout.slider.width)) * slider_ratio));
+    const slider_rail_y = layout.slider.y + slider_half_height - slider_rail_half_height;
+    try sink.PolyFillRectangle(drawable, gc, .initAssume(&[_]x11.Rectangle{
+        .{ .x = layout.slider.x, .y = slider_rail_y, .width = layout.slider.width, .height = slider_rail_half_height * 2 },
+        .{ .x = @intCast(slider_margin_left + slider_pos - 1), .y = 10, .width = 3, .height = slider_half_height * 2 },
+    }));
+
+    const margin = 50;
     var writer_buf: [16]u8 = undefined;
     var writer: Font.TextWriter = .init(.{
         .font = font,
@@ -217,14 +347,26 @@ fn render(
         .left_margin = margin,
         .buffer = &writer_buf,
     });
-
-    try writer.interface.print("Hello, {s}! These glyphs are missing: こんにちは", .{"World"});
     try writer.newline();
-    try writer.interface.writeAll("The quick brown fox jumped over the lazy dog");
+    try writer.interface.print("size: {d}", .{font_size});
+    try underline(&writer, margin);
+    try writer.newline();
+    if (font_file) |f| {
+        try writer.interface.print("{s}", .{f});
+    } else {
+        try writer.interface.writeAll("builtin font InterVariable.ttf");
+    }
+    try underline(&writer, margin);
+    try writer.newline();
+    try writer.interface.print("Hello, {s}! These glyphs are missing: こんにちは", .{"World"});
+    try underline(&writer, margin);
+    try writer.newline();
     try writer.newline();
     try writer.interface.writeAll("0123456789 ABCDEFGHIJKLMNOPQRSTUVWXYZ");
+    try underline(&writer, margin);
     try writer.newline();
     try writer.interface.writeAll("abcdefghijklmnopqrstuvwxyz");
+    try underline(&writer, margin);
     try writer.newline();
 
     writer.left_margin = 300;
@@ -266,6 +408,17 @@ fn render(
             .{ .window = window, .action = .background },
         })),
     }
+    return layout;
+}
+
+fn underline(
+    writer: *Font.TextWriter,
+    left: i16,
+) !void {
+    try writer.interface.flush();
+    try writer.sink.PolyFillRectangle(writer.drawable, writer.gc, .initAssume(&[_]x11.Rectangle{
+        .{ .x = left, .y = @intCast(writer.cursor.y), .width = @intCast(writer.cursor.x - left), .height = 1 },
+    }));
 }
 
 const Dbe = union(enum) {
@@ -282,6 +435,11 @@ const Dbe = union(enum) {
     }
 };
 
+fn errExit(comptime fmt: []const u8, args: anytype) noreturn {
+    std.log.err(fmt, args);
+    std.process.exit(0xff);
+}
+
 fn oom(e: error{OutOfMemory}) noreturn {
     @panic(@errorName(e));
 }
@@ -295,3 +453,4 @@ const ArenaAllocator = std.heap.ArenaAllocator;
 const FixedBufferAllocator = std.heap.FixedBufferAllocator;
 const Font = @import("Font");
 const TrueType = Font.TrueType;
+const Cmdline = @import("Cmdline.zig");
